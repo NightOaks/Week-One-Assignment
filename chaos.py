5e3f49850c4abfabe302ee013f64e2e7ba5e0c15
Devin Simoneaux <devin.simoneaux@myemail.indwes.edu>
2015-09-14 17:43:05 +0000

    Added the results.txt file and created a variable for loop counter and multiplier.

--- chaos.py
+++ chaos.py
@@ -4,10 +4,12 @@ __author__ = 'daniel.neumann'
 # A simple program illustrating chaotic behavior.
 
 def main():
+    loopcounter = 10
+    mult = 2.0
     print("This program illustrates a chaotic function")
     x = eval(input("Enter a number between 0 and 1: "))
-    for i in range(10):
-        x = 3.9 * x * (1 - x)
+    for i in range(loopcounter):
+        x = mult * x * (1 - x)
         print(x)
 
 main()
